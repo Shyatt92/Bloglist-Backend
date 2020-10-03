8e5740e8eca26fb5c3bd7b6f6e71a2abbf5f0199
Stephen Hyatt <shyatt92@gmail.com>
2020-10-03 13:14:25 +0100

    Added function to find author that created the most blogs, and added testing suite

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -26,9 +26,38 @@ const favoriteBlog = blogs => {
   return blogs[mostLiked]
 }
 
+const mostBlogs = blogs => {
+  let authors = [];
+
+  blogs.forEach(blog => {
+    let authorIndex = authors.findIndex(element => element.author === blog.author)
+    if(authorIndex === -1) {
+      authors.push({
+        author: blog.author,
+        blogs: 1
+      })
+    } else {
+      authors[authorIndex].blogs += 1
+    }
+  })
+
+  let mostBlogs = 0
+  let mostBlogged
+
+  authors.forEach((author, authorIndex) => {
+    if (author.blogs > mostBlogs) {
+      mostBlogs = author.blogs
+      mostBlogged = authorIndex
+    }
+  })
+
+  return authors[mostBlogged]
+}
+
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
