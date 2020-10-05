5be1d3287ea42d043cb3932a6568fbf99afc787b
Stephen Hyatt <shyatt92@gmail.com>
2020-10-05 14:50:22 +0100

    Set up node testing environment

--- utils/config.js
+++ utils/config.js
@@ -9,6 +9,10 @@ const mongoOptions = {
   useCreateIndex: true
 }
 
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
   MONGODB_URI,
   PORT,
